19bb064cd44bc7db7316d0afe8958837847255e9
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-26 15:22:01 -0300

    Requisito 6 e 7

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const btnAddTask = document.getElementById('criar-tarefa');
 const inputNewTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
+function selectTask() {
+  document.querySelectorAll('li').forEach((element) => {
+    element.addEventListener('click', (elementTarget) => {
+      const selectedTask = elementTarget.target;
+      selectedTask.style.backgroundColor = 'rgb(128,128,128)';
+    });
+  });
+}
+
 btnAddTask.addEventListener('click', () => {
   const newTask = document.createElement('li');
   newTask.innerText = inputNewTask.value;
   taskList.appendChild(newTask);
   inputNewTask.value = '';
+  selectTask();
 });
