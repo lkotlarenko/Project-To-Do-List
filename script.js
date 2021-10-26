d1b4ad244b691e4368ed0d62bfa116a33422d39a
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-26 15:42:02 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function selectTask() {
   document.querySelectorAll('li').forEach((element) => {
     element.addEventListener('click', (elementTarget) => {
       const selectedTask = elementTarget.target;
+      const liElement = document.querySelectorAll('li');
+      for (let i = 0; i < liElement.length; i += 1) {
+        if (liElement[i].style.backgroundColor === 'rgb(128, 128, 128)') {
+          liElement[i].removeAttribute('style');
+        }
+      }
       selectedTask.style.backgroundColor = 'rgb(128,128,128)';
     });
   });
